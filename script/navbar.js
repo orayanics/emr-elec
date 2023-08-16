4723ab7ce0daff7ba3646c4efd88122316f6b963
orayanics <nicole.oraya.cics@ust.edu.ph>
2023-08-16 17:46:31 +0800

    added footer
    included global.css

--- script/navbar.js
+++ script/navbar.js
@@ -1,6 +1,4 @@
 function toggleMenuMobile() {
-    console.log('wow andito ka')
     const mobileMenu = document.getElementById("mobile-items");
     mobileMenu.classList.toggle("hidden");
-    // mobileMenu.style.display = "block";
 }
